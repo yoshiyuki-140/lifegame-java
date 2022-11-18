72388a8dbfafb285dfa8ce2116d8167ea7aff337
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2022-11-18 10:00:06 +0900

    change ANSI esc code

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 /**
  * main
  */
-
 public class Main {
     public static void main(String[] args) {
         final int delay = 1000; // mill second
         LifeGame lg = new LifeGame();
         lg.printWorld();
         lg.createGrider(5, 5);
-        System.out.println("\u001b[2J");
+        // System.out.println("\u001b[2J");
         lg.printWorld();
 
         int count = 30;
         for (int i = 0; i < count; i++) {
-            // System.out.println("\u001b[2J");
-            System.out.println("\n");
+            // CLI画面を更新するために、ANSI文字コードで全画面削除
+            System.out.println(String.format("\u001b[2J", ));
+            // 
             lg.printWorld();
             lg.update();
             try {
